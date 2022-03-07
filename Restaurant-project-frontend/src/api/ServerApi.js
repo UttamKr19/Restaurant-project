a73b81e434210f862875cd60598e646e2e05587a
Uttam Kumar <uttam.kr.work@gmail.com>
2022-03-07 22:53:42 +0530

    changed server url in react

--- Restaurant-project-frontend/src/api/ServerApi.js
+++ Restaurant-project-frontend/src/api/ServerApi.js
@@ -1,5 +1,5 @@
-// const SERVER_API_BASE_URL=" https://webapp-ems2.herokuapp.com";
+const SERVER_API_BASE_URL="https://restaurant-backend-spring.herokuapp.com";
 
-const SERVER_API_BASE_URL="http://localhost:8090";
+// const SERVER_API_BASE_URL="http://localhost:8090";
 
 export default SERVER_API_BASE_URL;
